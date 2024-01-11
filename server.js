35c9bd1f36a6fc637083391881f5474656525c2d
imran122 <imransardar122@gmail.com>
2024-01-12 00:22:43 +0600

    first commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://law-firm-system-client.vercel.app"
+    "https://rent-ncar-client.onrender.com"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
